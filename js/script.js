a46b161d55b8f0d0118151042b7cf9f43eb82cec
JBSeng <leaguepower84616@gmail.com>
2019-11-13 02:24:46 +0000

    Attempted CpuChoice and results being shown

--- js/script.js
+++ js/script.js
@@ -4,10 +4,53 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+
 $("#shoot").click(function() {
     let inputVal=$('#input').val()
-    $("#userChoice").append(inputVal)
+    $("#userChoice").text(inputVal)
+
+    let cpuChoice = Math.random();
+    if (cpuChoice > .66) {
+        $('#computerChoice').text('rock');
+    }
+    else if (cpuChoice > .33 && cpuChoice < .66){
+        $('#computerChoice').text('paper');
+    }
+    else if (cpuChoice > 0 && cpuChoice < .33) {
+        $('#computerChoice').text('scissor');
+    }
+
+    if (inputVal === 'rock' && cpuChoice === 'scissor') {
+        $('#result').text('You win');
+    }
+    else if (inputVal ===  'rock' && cpuChoice === 'paper') {
+        $('#result').text('You Lose');
+    }
+    else if (inputVal === 'rock' && cpuChoice === 'rock'){
+        $('#result').text('You Tied');
+    }
+    else if (inputVal ===  'paper' && cpuChoice === 'rock') {
+        $('#result').text('You Win');
+    }
+     else if (inputVal ===  'paper' && cpuChoice === 'paper') {
+        $('#result').text('You Tied');
+    }
+     else if (inputVal ===  'paper' && cpuChoice === 'scissor') {
+        $('#result').text('You Win');
+    }
+     else if (inputVal ===  'scissor' && cpuChoice === 'paper') {
+        $('#result').text('You Win');
+    }
+     else if (inputVal ===  'scissor' && cpuChoice === 'rock') {
+        $('#result').text('You Lose');
+    }
+     else if (inputVal ===  'scissor' && cpuChoice === 'scissor') {
+        $('#result').text('You Tied');
+    }
+
 });
+
+
 
 
 // DOCUMENT READY FUNCTION BELOW
